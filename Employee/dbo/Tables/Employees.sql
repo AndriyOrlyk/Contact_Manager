252fd674b988ede3fcae78310570bf87be547932
AndriyOrlyk <orlykandriy@gmail.com>
2020-10-27 14:52:31 +0200

    Add Database in project ContactManager

---
+++ Employee/dbo/Tables/Employees.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [EmployeeId]  INT           IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (50) NOT NULL,
+    [DateOfBirth] DATETIME      NOT NULL,
+    [Married]     BIT           NOT NULL,
+    [Phone]       NVARCHAR (50) NOT NULL,
+    [Salary]      MONEY         NOT NULL,
+    CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED ([EmployeeId] ASC)
+);
+
